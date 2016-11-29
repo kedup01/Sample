ea2aec826dcfed46ebc172ac6368917b860c5379
Kiran Kumar Edupuganti <IN090020@applabs.com>
2016-11-29 19:15:40 +0530

    fresh

--- FirstJavaPro/src/com/company/Main.java
+++ FirstJavaPro/src/com/company/Main.java
@@ -2,12 +2,11 @@ package com.company;
 
 public class Main {
 
-    asdasda
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello this is sample");
         System.out.println("hello this is sample");
+        System.out.println("hello this is sample");
     }
 }
-21321
